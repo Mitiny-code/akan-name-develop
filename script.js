7e7549f8e0b1ce362df884860187e7de3f4a3e47
Mitiny-code <josephmitiny40@gmail.com>
2023-11-16 05:51:40 +0300

    fine tuned my html and started my javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+//function
+//getValues
+//output
+
+function generateAkanname() {
+    let year = document.getElementById("div4").value;
+    let month = document.getElementById("div5").value;
+    let date = document.getElementById("div6").value;
+}
