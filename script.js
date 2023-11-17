9e6807128c765dd6fda83c9bbd00f362fa713f30
Mitiny-code <josephmitiny40@gmail.com>
2023-11-17 10:51:06 +0300

    executed my function in javascript

--- script.js
+++ script.js
@@ -3,12 +3,19 @@
 //output
 
 let generateAkanName = function() {
+    const year = document.getElementById("year").value;
+    const month = document.getElementById("month").value;
+    const day = document.getElementById("day").value;
+    // const gender = document.getElementById("akanform").gender.value;
+   // const gender = "male"
     let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     let maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     const validDate = new Date(year, month, day);
     let dayOfTheWeek = validDate.getDay();
     const akanName = gender == "male" ? maleNames[dayOfTheWeek] : femaleNames[dayOfTheWeek];
-    console.log(akanName)
+    /*console.log(akanName);*/
+    alert(akanName);
+    
 }
 
 
