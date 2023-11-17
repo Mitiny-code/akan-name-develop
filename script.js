0624d197f618d3d2cbcda9bd4e5456833da4ef9c
Mitiny-code <josephmitiny40@gmail.com>
2023-11-17 10:13:12 +0300

    modified js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 //getValues
 //output
 
-let generateAkanName = function(year, month, day , gender) {
+let generateAkanName = function() {
     let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     let maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     const validDate = new Date(year, month, day);
     let dayOfTheWeek = validDate.getDay();
+    const akanName = gender == "male" ? maleNames[dayOfTheWeek] : femaleNames[dayOfTheWeek];
+    console.log(akanName)
 }
 
 
