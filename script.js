d8c3189587d7b7a21bddc29cbe6318c545227985
Mitiny-code <josephmitiny40@gmail.com>
2023-11-17 12:18:22 +0300

    linked my Html, Javascript and css

--- script.js
+++ script.js
@@ -4,18 +4,17 @@
 
 let generateAkanName = function() {
     const year = document.getElementById("year").value;
-    const month = document.getElementById("month").value;
+    const month = document.getElementById("month").value-1;
     const day = document.getElementById("day").value;
-    // const gender = document.getElementById("akanform").gender.value;
-   // const gender = "male"
+    const gender = document.getElementById("akanform").gender.value;
     let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     let maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     const validDate = new Date(year, month, day);
     let dayOfTheWeek = validDate.getDay();
-    const akanName = gender == "male" ? maleNames[dayOfTheWeek] : femaleNames[dayOfTheWeek];
-    /*console.log(akanName);*/
-    alert(akanName);
-    
+    console.log(gender)
+    const akanName = gender == "Male" ? maleNames[dayOfTheWeek] : femaleNames[dayOfTheWeek];
+    let message = "Your akan name is:";
+    alert(message + akanName);    
 }
 
 
